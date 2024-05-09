ab7593c9a722163827c6659afbdca7def4db0dbc
arBishal <m.arbishal@gmail.com>
2024-05-10 01:20:34 +0600

    Built basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,23 @@
 export default function Navbar() {
+// build todays date
   const date = new Date();
-  console.log(date);
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let month = months[date.getMonth()];
-  let today = `${date.getUTCDate()} ${month}, ${date.getFullYear()}`;
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  let today = `${weekday[date.getDay()]}, ${months[date.getMonth()]} ${date.getUTCDate()}, ${date.getFullYear()}`;
+  
+// specify greetings based on hour
+let hour = date.getHours();
+console.log(hour);
+let greetings = "";
+if(hour>18 || hour<6) greetings = "Hey there, Night Owl!"
+else greetings = "Hey there, Sunflower!"
+
+  
   return (
     <div className="navbar-outer">
       <div className="navbar-inner">
-        <span className="font-bold text-2xl">What-To-Do</span>
+        <span className="mt-1 p-0"> {greetings} </span>
+        <span className="font-bold text-2xl">What-To-Do-?</span>
         <span className="mt-1 p-0"> {today} </span>
       </div>
     </div>
