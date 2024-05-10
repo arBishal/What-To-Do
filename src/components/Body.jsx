90c775fbe6e9c1a78688a1e92478e63137736d28
arBishal <m.arbishal@gmail.com>
2024-05-11 03:09:35 +0600

    Fix navbar position

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,7 @@ import NoTodoCard from "./ui/NoTodoCard";
 
 export default function Body() {
     return(
-        <div className="body">
+        <div className="bg-neutral-50 w-full h-full px-6 pb-4 pt-24 sm:pt-20 flex flex-col justify-start items-center">
             <NoTodoCard/>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
   else greetings = "Hey there, Sunflower!";
 
   return (
-    <div className="border-b-[1px] bg-white px-6 py-4 flex justify-center align-middle">
+    <div className="border-b-[1px] bg-white w-full px-6 py-4 flex justify-center align-middle fixed">
       <div className="max-w-screen-md w-full text-black flex flex-row gap-2 items-center justify-between">
         <span className="font-extrabold text-xl sm:text-2xl text-center order-1 sm:order-2">
           WhatToDo?
