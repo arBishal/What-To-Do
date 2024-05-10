9183d347a6f18d0e8a546924111f8e6f22f0094d
arBishal <m.arbishal@gmail.com>
2024-05-11 03:56:55 +0600

    Fix plus icon position

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,7 +6,9 @@ export default function Body() {
   return (
     <div className="bg-neutral-50 w-full h-full px-6 pb-4 pt-24 sm:pt-20 flex flex-col justify-start items-center">
       <NoTodoCard />
-      <PlusIcon />
+      <span className="fixed bottom-6 cursor-pointer">
+        <PlusIcon />
+      </span>
     </div>
   );
 }
