36743feb61cd196bcf61c2c5101fe6540bbfc3d5
arahman <ashikur.rahman@dsinnovators.com>
2024-06-27 17:54:28 +0600

    Make capsule colorful

--- src/components/ui/cards/Capsule.jsx
+++ src/components/ui/cards/Capsule.jsx
@@ -1,6 +1,38 @@
 export const Capsule = ({ priority }) => {
+  
+  const getStyles = (priority) => {
+    switch (priority) {
+      case 'high':
+        return {
+          bgColor: 'bg-red-100',
+          textColor: 'text-red-700',
+          borderColor: 'border-red-300',
+        };
+      case 'medium':
+        return {
+          bgColor: 'bg-amber-100',
+          textColor: 'text-amber-700',
+          borderColor: 'border-amber-300',
+        };
+      case 'low':
+        return {
+          bgColor: 'bg-green-100',
+          textColor: 'text-green-700',
+          borderColor: 'border-green-300',
+        };
+      default:
+        return {
+          bgColor: 'bg-neutral-100',
+          textColor: 'text-neutral-500',
+          borderColor: 'border-neutral-300',
+        };
+    }
+  };
+  
+  const { bgColor, textColor, borderColor } = getStyles(priority);
+
   return (
-    <span className="text-sm text-neutral-500 px-1 border-2 border-neutral-300 rounded bg-neutral-100">
+    <span className={`text-sm ${textColor} px-1 border-2 ${borderColor} rounded ${bgColor}`}>
       {priority}
     </span>
   );
